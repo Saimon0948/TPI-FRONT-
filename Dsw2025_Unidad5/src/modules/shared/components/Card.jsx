8cecd8075e71ffc4d0d293e7bef28c7fce14987d
Saimon0948 <Luis.Bravo@alu.frt.utn.edu.ar>
2025-11-19 20:48:20 -0300

    Delete Dsw2025_Unidad5 directory

--- Dsw2025_Unidad5/src/modules/shared/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-function Card({ children, className }) {
-  return (
-    <div className={`bg-white border border-gray-300 p-4 rounded-xl ${className}`}>
-      {children}
-    </div>
-  );
-};
-
-export default Card;
